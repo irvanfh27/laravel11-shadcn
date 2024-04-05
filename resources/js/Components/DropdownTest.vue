ab49d3a89678c4c395743e4dbd4b4fa4cdcc7873
Shiveryn <irvanfathul@gmail.com>
2024-04-05 10:10:25 +0700

    add component

---
+++ resources/js/Components/DropdownTest.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { cn } from '@/lib/utils.js'
+import NavLink from "@/Components/NavLink.vue";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from '@/Components/ui/dropdown-menu'
+</script>
+
+<template>
+    <DropdownMenu
+    >
+        <DropdownMenuTrigger
+            class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+        >
+            Master
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <!--                <DropdownMenuLabel>My Account</DropdownMenuLabel>-->
+            <!--                <DropdownMenuSeparator />-->
+            <DropdownMenuItem>Pegawai</DropdownMenuItem>
+            <DropdownMenuItem>Non Pegawai</DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+</template>
+
+<style scoped>
+
+</style>
